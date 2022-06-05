91445a5bc92c08a38167f92baa436b21cc69966d
krisstina642 <45144811+krisstina642@users.noreply.github.com>
2022-06-05 02:40:34 +0200

    Initial UI added

--- src/main/scala/hello/Bloxxrz.scala
+++ src/main/scala/hello/Bloxxrz.scala
@@ -11,6 +11,7 @@ import scalafx.beans.property.ObjectProperty
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.layout.VBox
 import scalafx.scene.shape.Rectangle
+import scalafx.stage.Stage
 
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
@@ -20,10 +21,21 @@ object Bloxxrz extends JFXApp3 {
   val step=40
   val RED = new Color(255,0,0)
   val BLACK = new Color(0,0,0)
-  val WHITE = new Color(255,255,255)
+  val LIGHT_GREY = new Color(150,150,150)
   val BLUE = new Color(0, 0, 255)
   val GREY = new Color(100,100,100)
   var initialBlox:List[(Double, Double)]=null;
+
+  def createButton(text_ :String, function: Unit):Button ={
+    new Button {
+      text = text_
+      prefWidth = 200.0
+      prefHeight = 30.0
+      onAction = (e: ActionEvent) => {
+        function
+      }
+    }
+  }
 
   def createRect(xr:Double, yr:Double, color: Color) = new Rectangle{
     x=xr
@@ -53,7 +65,7 @@ object Bloxxrz extends JFXApp3 {
           case 'O' => rec += createRect( i*step, j*step , GREY)
           case 'S' => rec += createRect(i*step, j*step , GREY)
           case 'T' => rec += createRect( i*step, j*step , RED)
-          case '.' => rec += createRect( i*step, j*step , WHITE)
+          case '.' => rec += createRect( i*step, j*step , LIGHT_GREY)
         }
     }
     rec.toList
@@ -75,7 +87,6 @@ object Bloxxrz extends JFXApp3 {
           case _ => {}
         }
     }
-    System.out.println(initialBlox.length)
     }
     (initialBlox.toList, endPos, empty.toList,special.toList)
   }
@@ -110,8 +121,10 @@ object Bloxxrz extends JFXApp3 {
       }
 
       val newBlox :List[(Double,Double)]=
-        if (newx1 < 0 || newx1 > 600 || newy1 < 0 || newy1 > 600 ||
-          newx2 < 0 || newx2 > 600 || newy2 < 0 || newy2 > 600) {
+        if (newx1 < 0 || newx1 >= 600 || newy1 < 0 || newy1 >= 600 ||
+          newx2 < 0 || newx2 >= 600 || newy2 < 0 || newy2 >= 600 ||
+          (newx1==newx2 && newy1==newy2 && specialBloxList.contains((newx1,newy1))) ||
+          emptyBloxList.contains((newx1,newy1)) || emptyBloxList.contains((newx2,newy2))) {
           System.out.println("end game")
           initialBlox
         } /// end game
@@ -124,7 +137,6 @@ object Bloxxrz extends JFXApp3 {
         }
       State(content, newBlox, endPos, emptyBloxList, specialBloxList)
     }
-
     def rectangles: List[Rectangle]= {
       getRectangles(content) ::: blox.map { case (x, y) => createRect(x, y, BLUE) }
     }
@@ -157,12 +169,36 @@ object Bloxxrz extends JFXApp3 {
     }
   }
 
-  override def start(): Unit = {
-    val game_settings=  ObjectProperty(GameSettings("example.txt"))
-    getRectangles(game_settings.value.content)
-    val (blox, end, empty, spec)=getBlocks(game_settings.value.content);
+  def startLevel(stage: Stage): Unit ={
+    val game_settings= ObjectProperty(GameSettings("example.txt"))
+    val arena=game_settings.value.content
+    getRectangles(arena)
+    val (blox, end, empty, spec)=getBlocks(arena);
     initialBlox = blox
-    val state=  ObjectProperty(State(game_settings.value.content,blox, end, empty, spec))
+    val state=  ObjectProperty(State(arena,blox, end, empty, spec))
+    stage.scene.value.content = state.value.rectangles
+    var direct = 1;
+    stage.scene.value.onKeyPressed = key => {
+
+      state.update(state.value.newState(
+        key.getCode match {
+        case KeyCode.W => 1
+        case KeyCode.S => 2
+        case KeyCode.A =>  3
+        case KeyCode.D =>  4
+        case KeyCode.UP =>  1
+        case KeyCode.DOWN =>  2
+        case KeyCode.LEFT =>  3
+        case KeyCode.RIGHT =>  4
+        case _ => System.out.println(" WRONG KEY ")
+          -1
+      }))
+      stage.scene.value.content = state.value.rectangles
+    }
+  }
+
+  override def start(): Unit = {
+
     stage = new JFXApp3.PrimaryStage {
       width = 600
       height = 600
@@ -172,23 +208,20 @@ object Bloxxrz extends JFXApp3 {
         text= "Start"
         prefWidth = 200.0
         prefHeight = 30.0
-      }
-      button_start.onAction = (e: ActionEvent) =>{
-        stage.close()
+        onAction = (e: ActionEvent) => startLevel(stage)
       }
 
       val button_quit= new Button{
         text= "Quit"
         prefWidth = 200.0
         prefHeight = 30.0
+        onAction = (e: ActionEvent) => stage.close()
       }
-      button_quit.onAction = (e: ActionEvent) =>{
-        stage.close()
-      }
+
       title = "Bloxxrz"
       scene = new Scene {
         fill = Color.rgb(38, 38, 38)
-        content = state.value.rectangles
+     /*   content = state.value.rectangles
         var direct = 1;
         onKeyPressed = key => {
           key.getCode match {
@@ -204,15 +237,15 @@ object Bloxxrz extends JFXApp3 {
         }
           state.update(state.value.newState(direct))
           content = state.value.rectangles
-        }
-      /*  content = new VBox{
+        }*/
+       content = new VBox{
           prefWidth = 600
           prefHeight = 600
           padding = Insets(50, 70, 70, 50)
           alignment = Pos.BaselineCenter
           alignmentInParent= Pos.BaselineCenter
           children=Seq(button_start, button_quit)
-        }*/
+        }
       /*  val hbox = new HBox {
           padding = Insets(50, 80, 50, 80)
           children = Seq(
